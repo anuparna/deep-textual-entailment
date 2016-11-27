57e06c6481615ef83fb0a49b4a07ec39988e7168
Rahul <rahulhuilgol@gmail.com>
2016-11-27 04:09:15 -0600

    experiments files

--- src/experiments_concat.py
+++ src/experiments_concat.py
@@ -4,7 +4,10 @@ w2vec = w2vec.W2Vec()
 snli = data_snli.SNLI(w2vec)
 
 import concat_lstm
-c = concat_lstm.ConcatLstm(snli,3,50,64,batch_size=50000)
-c.train()
-c.model.save('concat_50lstm_64dense_50kbatch.h5')
-c.test()
+
+for lstmsize in [10,20,30,50]:
+	for densesize in [20, 32, 64, 128]:
+		c = concat_lstm.ConcatLstm(snli, 3, lstmsize, densesize, batch_size=10000)
+		c.train()
+		c.model.save('../experiments/models/concat_'+str(lstmsize)+'lstm_'+str(densesize)+'dense_10kbatch.h5')
+		c.test()
--- src/experiments_shared.py
+++ src/experiments_shared.py
@@ -4,7 +4,10 @@ w2vec = w2vec.W2Vec()
 snli = data_snli.SNLI(w2vec)
 
 import shared_encoding
-c = shared_encoding.SharedEncoding(snli,3,50,64,batch_size=10000)
-c.train()
-c.model.save('shared_50lstm_64dense_10kbatch.h5')
-c.test()
+
+for lstmsize in [10,20,30,50]:
+	for densesize in [20, 32, 64, 128]:
+		c = shared_encoding.SharedEncoding(snli, 3, lstmsize, densesize, batch_size=10000)
+		c.train()
+		c.model.save('../experiments/models/shared_'+str(lstmsize)+'lstm_'+str(densesize)+'dense_10kbatch.h5')
+		c.test()
