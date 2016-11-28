f3e6b1f9d58fbb2053c8337445d6e52a75be2bc1
Rahul huilgol <rahulhuilgol@gmail.com>
2016-11-28 02:51:55 +0000

    fix generator bug finally

--- src/experiments_concat.py
+++ src/experiments_concat.py
@@ -7,6 +7,8 @@ import concat_lstm
 
 for lstmsize in [10,20,30,50]:
 	for densesize in [20, 32, 64, 128]:
+		if lstmsize==10 and densesize==20:
+			continue
 		c = concat_lstm.ConcatLstm(snli, 3, lstmsize, densesize, batch_size=10000)
 		c.train()
 		c.model.save('../experiments/models/concat_'+str(lstmsize)+'lstm_'+str(densesize)+'dense_10kbatch.h5')
